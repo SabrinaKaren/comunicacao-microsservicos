2ff125565e8e7dc0a9b2d3ca4a0b198d358c442f
SabrinaKaren <sabrinaa.karen@hotmail.com>
2022-04-26 11:29:56 -0300

    Product-api: inicialização da api

---
+++ product-api/src/test/java/br/com/cursomicrosservicos/productapi/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cursomicrosservicos.productapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
